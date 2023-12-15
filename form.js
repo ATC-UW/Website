2af1067bac7fc851e7eec77beb3f53f7548572ad
Bhavesh <bkumar2@cs.washington.edu>
2023-12-14 18:07:20 -0800

    form logic

--- form.js
+++ form.js
@@ -1,11 +1,13 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxgRcdi_3LSnc4M6K4UfrMt2HXPPCIGwA5-2eTEUMjsB6NoFMNOtRZ3WyQPhzD5LBTQFw/exec'
 
-const form = document.forms['contact-form']
+const form = document.getElementById('user-form');
+console.log(form)
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
+form.addEventListener("submit", e => {
+    console.log(e);
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => alert("Thank you! your form is submitted successfully." ))
+    .then(() => { window.location.reload(); })
+    .catch(error => console.error('Error!', error.message))
 })
